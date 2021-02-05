0e9460a4398d6faef6c5127761527f12ba161995
Luthien95 <aleksandrasalak@gmail.com>
2021-02-05 23:37:22 +0100

    Add node js

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+var posts = [{ message: "hello" }, { message: "hi" }];
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.listen(3000);
